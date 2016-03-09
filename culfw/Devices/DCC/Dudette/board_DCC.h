1e762686e8fb1d7178f9145d4fbf32c75643200a
Stephan Eisler <stephan@eisler.de>
2016-03-09 19:23:13 +0100

    initial settings

--- culfw/Devices/DCC/Dudette/board_DCC.h
+++ culfw/Devices/DCC/Dudette/board_DCC.h
@@ -1,4 +1,4 @@
-#ifndef _BOARD_COC_H
+fndef _BOARD_COC_H
 #define _BOARD_COC_H
 
 /*********************************/
@@ -16,21 +16,30 @@
 
 /* where is the conditional switch located? */
 
-#define BOOT_PORT               PORTB
-#define BOOT_DDR                DDRB
-#define BOOT_IN                 PINB
-#define BOOT_PIN                3
+#define BOOT_PORT               PORTC
+#define BOOT_DDR                DDRC
+#define BOOT_IN                 PINC
+#define BOOT_PIN                0
 #define BOOT_LOWACTIVE
 
-#define SWITCH_PORT             PORTB
-#define SWITCH_DDR              DDRB
-#define SWITCH_IN               PINB
-#define SWITCH_PIN              0
+#define SWITCH_PORT             PORTC
+#define SWITCH_DDR              DDRC
+#define SWITCH_IN               PINC
+#define SWITCH_PIN              1
 #define SWITCH_LOWACTIVE
 
 #define LED_PORT                PORTC
 #define LED_DDR                 DDRC
 #define LED_PIN                 6
 
+#define LED1_PORT                PORTC
+#define LED1_DDR                 DDRC
+#define LED1_PIN                 7
+
+#define LED2_PORT                PORTD
+#define LED2_DDR                 DDRD
+#define LED2_PIN                 4
+
 #endif
+
 
